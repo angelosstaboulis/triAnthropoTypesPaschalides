1e5cc876ab9332538319284d68b4fd385e9fc32f
Angelos Staboulis <angelosstaboulis@gmail.com>
2023-06-09 13:42:59 +0300

    Initial Commit tri-anthropo-types-Paschalides

---
+++ trianthropotypePaschalides/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trianthropotypePaschalides
+//
+//  Created by Angelos Staboulis on 3/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trianthropotypePaschalides/trianthropotypePaschalidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  trianthropotypePaschalidesApp.swift
+//  trianthropotypePaschalides
+//
+//  Created by Angelos Staboulis on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct trianthropotypePaschalidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
